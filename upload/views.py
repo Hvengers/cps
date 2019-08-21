db14ebaded3869c03fed53d2c21f2939915d09e0
hvengers <a01038308569@gmail.com>
2019-08-21 11:49:44 +0900

    upload

--- upload/views.py
+++ upload/views.py
@@ -13,32 +13,84 @@ from django.contrib.auth.models import User
 from rest_framework import permissions, renderers, viewsets
 #from rest_framework.decorators import detail_route
 from rest_framework.response import Response
-
+import time
 #from blog.permissions import IsOwnerOrReadOnly
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
-    """
-    This viewset automatically provides `list`, `create`, `retrieve`,
-    `update` and `destroy` actions.
-    Additionally we also provide an extra `highlight` action.
-    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    #permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly, )
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    This viewset automatically provides `list` and `detail` actions.
-    """
     queryset = User.objects.all()
-#    serializer_class = UserSerializer
+
+
 
 global safeNum
 
-
+safeNum=0
+count=0
 def upload(req):
+    global count
+
+
+    if 'isTerminated' in req.POST:
+        file_loc="C:/Users/jineok/Desktop/djangogirls/revised_server/blog/static/image/"
+        src=file_loc+'inner_temp.jpg'
+        dest=file_loc+'inner.jpg'
+        shutil.copy(src,dest)
+        src=file_loc+'crime_temp.jpg'
+        dest=file_loc+'crime.jpg'
+        shutil.copy(src,dest)
+
+
+        file_loc= 'blog/static/info'
+        fp_taxi = open('%s/%s' % (file_loc,'taxi_info.txt') , 'w+',encoding='UTF8')
+        fp_crime = open('%s/%s' % (file_loc,'crime_info.txt') , 'w+',encoding='UTF8')
+
+
+
+        fp_taxi.write(' ')
+        fp_crime.write(' ')
+
+
+        fp_taxi.close()
+        fp_crime.close()
+
+
+
+        fp_pay = open('%s/%s' % (file_loc,'pay_info.txt') , 'r+',encoding='UTF8')
+
+        data_dic={}
+
+
+        if 'payCode' in req.POST:
+            payCode=req.POST['payCode']
+            payInfo=req.POST['payInfo']
+            paySum=req.POST['paySum']
+
+            fp_pay.write(payCode)
+            fp_pay.write('\n')
+            fp_pay.write(payInfo)
+            fp_pay.write('\n')
+            fp_pay.write(paySum)
+            fp_pay.write('\n')
+        else:
+            payCode=fp_pay.readline()
+            payInfo=fp_pay.readline()
+            paySum=fp_pay.readline()
+
+
+        fp_pay.close()
+
+
+        data_dic['payCode']=payCode
+        data_dic['payInfo']=payInfo
+        data_dic['paySum']=paySum
+
+
+        return render(req, 'blog/post_list.html',data_dic)
 
     if 'reset' in req.POST:
 
@@ -53,16 +105,17 @@ def upload(req):
         fp_taxi = open('%s/%s' % (file_loc,'taxi_info.txt') , 'r+',encoding='UTF8')
         fp_crime = open('%s/%s' % (file_loc,'crime_info.txt') , 'r+',encoding='UTF8')
 
-        '''template으로 보낼 딕셔너리 초기화'''
+
         data_dic={}
-        '''택시 정보'''
+
         if 'taxiNumber' in req.POST:     #하나만 들어오진 않고, 무조건 다 들어오니 하나만 체크하면 됨.
             taxiNumber=req.POST['taxiNumber']
             taxiDriver=req.POST['taxiDriver']
             taxiGPS=req.POST['taxiGPS']
             taxiReportCode=req.POST['taxiReportCode']
+            taxiLatitude=req.POST['taxiLatitude']
+            taxiLongitude=req.POST['taxiLongitude']
 
-            #print("always get")
             fp_taxi.write(taxiNumber)
             fp_taxi.write('\n')
             fp_taxi.write(taxiDriver)
@@ -71,14 +124,20 @@ def upload(req):
             fp_taxi.write('\n')
             fp_taxi.write(taxiReportCode)
             fp_taxi.write('\n')
+            fp_taxi.write(taxiLatitude)
+            fp_taxi.write('\n')
+            fp_taxi.write(taxiLongitude)
+            fp_taxi.write('\n')
         else:
-            #print("not always get")
+
             taxiNumber=fp_taxi.readline()
             taxiDriver=fp_taxi.readline()
             taxiGPS=fp_taxi.readline()
             taxiReportCode=fp_taxi.readline()
+            taxiLatitude=fp_taxi.readline()
+            taxiLongitude=fp_taxi.readline()
 
-            '''범죄자 정보'''
+
         if 'criminalCode' in req.POST:
             criminalCode=req.POST['criminalCode']
             criminalName=req.POST['criminalName']
@@ -105,7 +164,7 @@ def upload(req):
             criminalGender=fp_crime.readline()
             criminalHeight=fp_crime.readline()
             criminalAddress=fp_crime.readline()
-            '''결제 정보'''
+
 
         fp_taxi.close()
         fp_crime.close()
@@ -114,6 +173,8 @@ def upload(req):
         data_dic['taxiDriver']=taxiDriver
         data_dic['taxiGPS']=taxiGPS
         data_dic['taxiReportCode']=taxiReportCode
+        data_dic['taxiLatitude']=taxiLatitude
+        data_dic['taxiLongitude']=taxiLongitude
         data_dic['criminalCode']=criminalCode
         data_dic['criminalName']=criminalName
         data_dic['criminalBirth']=criminalBirth
@@ -123,55 +184,30 @@ def upload(req):
 
         return render(req, 'blog/post_list.html',data_dic)
 
-    global safeNum
-    #flag_safe=0
-    '''safe한 경우 처리 플래그'''
+
     if 'safe' in req.POST:
         flag_safe=1
-        #print(repr(serializer))
-#        if 'title != safe\n' in req.GET('http://hvenger.pythonanywhere.com/blog/'):
-        '''
-        serializer = SnippetSerializer(Snippet(title='aa'))
-        print(serializer.data)
-        print('hello')
-        snippet = Snippet(title='safe\n')
-        snippet.save()
-        #snippet.delete()
-        #r=requests.delete('http://127.0.0.1:8000/blog/none/')
-        #print(r)
-        serializer = SnippetSerializer(snippet)
-        print(serializer.data)
-        content = JSONRenderer().render(serializer.data)
-        '''
-        #r = requests.get('http://127.0.0.1:8000/blog/1/')
-        #print(r)
-        #r=requests.get(url='http://127.0.0.1:8000/blog/')
-        #data =r.json()
-        safeNum=0
-        if 'isSafe' in req.POST:     #하나만 들어오진 않고, 무조건 다 들어오니 하나만 체크하면 됨.
-            safeNum=req.POST['isSafe']
-            safeNum=int(safeNum)
-            if safeNum != 1:
-                snippet = Snippet(title='safe\n')
-                snippet.save()
-                serializer = SnippetSerializer(snippet)
-                content = JSONRenderer().render(serializer.data)
+        global safeNum
+
+        if safeNum != 1:
+            snippet = Snippet(title='safe')
+            snippet.save()
+            serializer = SnippetSerializer(snippet)
+            content = JSONRenderer().render(serializer.data)
+            safeNum= 1
+
+
+        count=1
+
 
 
     else: #'refresh' in req.POST:
         flag_safe=0
 
-        #r = requests.delete('http://hvenger.pythonanywhere.com/blog/')
-        #print(r)
 
-#    if req.method == 'POST':
-#    for afile in req.FILES.getlist('files'):
-#        File(file=afile, files=test).save()
-    '''safe한 경우에는 기본이미지 띄우고 변수안넘겨줌'''
     if flag_safe ==1:
-        #print("hi")
-        '''temp로 평소에 띄울 사진 유지시켜놔야됨'''
-        file_loc="/home/Hvenger/cps/blog/static/image/"
+
+        file_loc="C:/Users/jineok/Desktop/djangogirls/revised_server/blog/static/image/"
         src=file_loc+'inner_temp.jpg'
         dest=file_loc+'inner.jpg'
         shutil.copy(src,dest)
@@ -234,119 +270,141 @@ def upload(req):
 
         '''refresh하면 정상적으로 작동'''
     else:
-        '''이미지 파일처리'''
-        if 'file' in req.FILES:
-            file = req.FILES['file']
-            filename = file._name
-            file_loc='blog/static/image'
-            #fp = open('%s/%s' % (file_loc, 'inner.jpg') , 'wb')
-            #print(filename)
-            '''실내인지 범죄자인지 사진 구분'''
-            if filename == 'taxiInternalPhoto.jpg':
-                fp = open('%s/%s' % (file_loc, 'inner.jpg') , 'wb')
-                #print("done")
-            elif filename == 'criminalPhoto.jpg':
-                fp = open('%s/%s' % (file_loc, 'crime.jpg') , 'wb')
+        if count != 1:
+            '''이미지 파일처리'''
+            if 'file' in req.FILES:
+                file = req.FILES['file']
+                filename = file._name
+                file_loc='blog/static/image'
+                #fp = open('%s/%s' % (file_loc, 'inner.jpg') , 'wb')
+                #print(filename)
+                '''실내인지 범죄자인지 사진 구분'''
+                if filename == 'taxiInternalPhoto.jpg':
+                    fp = open('%s/%s' % (file_loc, 'inner.jpg') , 'wb')
+                    #print("done")
+                elif filename == 'criminalPhoto.jpg':
+                    fp = open('%s/%s' % (file_loc, 'crime.jpg') , 'wb')
 
-            for chunk in file.chunks():
-                fp.write(chunk)
+                for chunk in file.chunks():
+                    fp.write(chunk)
 
-            fp.close()
+                fp.close()
 
-        file_loc='blog/static/info'
-        fp_taxi = open('%s/%s' % (file_loc,'taxi_info.txt') , 'r+',encoding='UTF8')
-        fp_crime = open('%s/%s' % (file_loc,'crime_info.txt') , 'r+',encoding='UTF8')
-        fp_pay = open('%s/%s' % (file_loc,'pay_info.txt') , 'r+',encoding='UTF8')
-        # w+로 읽기전에 열면 파일 내용 지워 버림. r+가 적당. 모두 파일 스트림 시작에 위치
-        fp_safe = open('%s/%s' % (file_loc,'safe_info.txt') , 'r+',encoding='UTF8')
+            file_loc='blog/static/info'
+            fp_taxi = open('%s/%s' % (file_loc,'taxi_info.txt') , 'r+',encoding='UTF8')
+            fp_crime = open('%s/%s' % (file_loc,'crime_info.txt') , 'r+',encoding='UTF8')
+            fp_pay = open('%s/%s' % (file_loc,'pay_info.txt') , 'r+',encoding='UTF8')
+            # w+로 읽기전에 열면 파일 내용 지워 버림. r+가 적당. 모두 파일 스트림 시작에 위치
+            fp_safe = open('%s/%s' % (file_loc,'safe_info.txt') , 'r+',encoding='UTF8')
 
 
-        '''template으로 보낼 딕셔너리 초기화'''
-        data_dic={}
-        '''택시 정보'''
-        if 'taxiNumber' in req.POST:     #하나만 들어오진 않고, 무조건 다 들어오니 하나만 체크하면 됨.
-            taxiNumber=req.POST['taxiNumber']
-            taxiDriver=req.POST['taxiDriver']
-            taxiGPS=req.POST['taxiGPS']
-            taxiReportCode=req.POST['taxiReportCode']
+            '''template으로 보낼 딕셔너리 초기화'''
+            data_dic={}
+            '''택시 정보'''
 
-            #print("always get")
-            fp_taxi.write(taxiNumber)
-            fp_taxi.write('\n')
-            fp_taxi.write(taxiDriver)
-            fp_taxi.write('\n')
-            fp_taxi.write(taxiGPS)
-            fp_taxi.write('\n')
-            fp_taxi.write(taxiReportCode)
-            fp_taxi.write('\n')
-        else:
-            #print("not always get")
-            taxiNumber=fp_taxi.readline()
-            taxiDriver=fp_taxi.readline()
-            taxiGPS=fp_taxi.readline()
-            taxiReportCode=fp_taxi.readline()
+
+            if 'taxiNumber' in req.POST:     #하나만 들어오진 않고, 무조건 다 들어오니 하나만 체크하면 됨.
+                safeNum=0
+                taxiNumber=req.POST['taxiNumber']
+                taxiDriver=req.POST['taxiDriver']
+                taxiGPS=req.POST['taxiGPS']
+                taxiReportCode=req.POST['taxiReportCode']
+                taxiLatitude=req.POST['taxiLatitude']
+                taxiLongitude=req.POST['taxiLongitude']
+
+                #print("always get")
+                fp_taxi.write(taxiNumber)
+                fp_taxi.write('\n')
+                fp_taxi.write(taxiDriver)
+                fp_taxi.write('\n')
+                fp_taxi.write(taxiGPS)
+                fp_taxi.write('\n')
+                fp_taxi.write(taxiReportCode)
+                fp_taxi.write('\n')
+                fp_taxi.write(taxiLatitude)
+                fp_taxi.write('\n')
+                fp_taxi.write(taxiLongitude)
+                fp_taxi.write('\n')
+            else:
+                #print("not always get")
+                taxiNumber=fp_taxi.readline()
+                taxiDriver=fp_taxi.readline()
+                taxiGPS=fp_taxi.readline()
+                taxiReportCode=fp_taxi.readline()
+                taxiLatitude=fp_taxi.readline()
+                taxiLongitude=fp_taxi.readline()
 
             '''범죄자 정보'''
-        if 'criminalCode' in req.POST:
-            criminalCode=req.POST['criminalCode']
-            criminalName=req.POST['criminalName']
-            criminalBirth=req.POST['criminalBirth']
-            criminalGender=req.POST['criminalGender']
-            criminalHeight=req.POST['criminalHeight']
-            criminalAddress=req.POST['criminalAddress']
-            fp_crime.write(criminalCode)
-            fp_crime.write('\n')
-            fp_crime.write(criminalName)
-            fp_crime.write('\n')
-            fp_crime.write(criminalBirth)
-            fp_crime.write('\n')
-            fp_crime.write(criminalGender)
-            fp_crime.write('\n')
-            fp_crime.write(criminalHeight)
-            fp_crime.write('\n')
-            fp_crime.write(criminalAddress)
-            fp_crime.write('\n')
-        else:
-            criminalCode=fp_crime.readline()
-            criminalName=fp_crime.readline()
-            criminalBirth=fp_crime.readline()
-            criminalGender=fp_crime.readline()
-            criminalHeight=fp_crime.readline()
-            criminalAddress=fp_crime.readline()
+            if 'criminalCode' in req.POST:
+                safeNum=0
+                criminalCode=req.POST['criminalCode']
+                criminalName=req.POST['criminalName']
+                criminalBirth=req.POST['criminalBirth']
+                criminalGender=req.POST['criminalGender']
+                criminalHeight=req.POST['criminalHeight']
+                criminalAddress=req.POST['criminalAddress']
+                fp_crime.write(criminalCode)
+                fp_crime.write('\n')
+                fp_crime.write(criminalName)
+                fp_crime.write('\n')
+                fp_crime.write(criminalBirth)
+                fp_crime.write('\n')
+                fp_crime.write(criminalGender)
+                fp_crime.write('\n')
+                fp_crime.write(criminalHeight)
+                fp_crime.write('\n')
+                fp_crime.write(criminalAddress)
+                fp_crime.write('\n')
+            else:
+                criminalCode=fp_crime.readline()
+                criminalName=fp_crime.readline()
+                criminalBirth=fp_crime.readline()
+                criminalGender=fp_crime.readline()
+                criminalHeight=fp_crime.readline()
+                criminalAddress=fp_crime.readline()
             '''결제 정보'''
-        if 'payCode' in req.POST:
-            payCode=req.POST['payCode']
-            payInfo=req.POST['payInfo']
-            paySum=req.POST['paySum']
+            if 'payCode' in req.POST:
+                payCode=req.POST['payCode']
+                payInfo=req.POST['payInfo']
+                paySum=req.POST['paySum']
 
-            fp_pay.write(payCode)
-            fp_pay.write('\n')
-            fp_pay.write(payInfo)
-            fp_pay.write('\n')
-            fp_pay.write(paySum)
-            fp_pay.write('\n')
-        else:
-            payCode=fp_pay.readline()
-            payInfo=fp_pay.readline()
-            paySum=fp_pay.readline()
-
-        fp_taxi.close()
-        fp_crime.close()
-        fp_pay.close()
-
-        data_dic['taxiNumber']=taxiNumber
-        data_dic['taxiDriver']=taxiDriver
-        data_dic['taxiGPS']=taxiGPS
-        data_dic['taxiReportCode']=taxiReportCode
-        data_dic['criminalCode']=criminalCode
-        data_dic['criminalName']=criminalName
-        data_dic['criminalBirth']=criminalBirth
-        data_dic['criminalGender']=criminalGender
-        data_dic['criminalHeight']=criminalHeight
-        data_dic['criminalAddress']=criminalAddress
-        data_dic['payCode']=payCode
-        data_dic['payInfo']=payInfo
-        data_dic['paySum']=paySum
+                fp_pay.write(payCode)
+                fp_pay.write('\n')
+                fp_pay.write(payInfo)
+                fp_pay.write('\n')
+                fp_pay.write(paySum)
+                fp_pay.write('\n')
+            else:
+                payCode=fp_pay.readline()
+                payInfo=fp_pay.readline()
+                paySum=fp_pay.readline()
 
 
-        return render(req, 'blog/post_list.html',data_dic)
+
+
+
+            fp_taxi.close()
+            fp_crime.close()
+            fp_pay.close()
+
+            data_dic['taxiNumber']=taxiNumber
+            data_dic['taxiDriver']=taxiDriver
+            data_dic['taxiGPS']=taxiGPS
+            data_dic['taxiReportCode']=taxiReportCode
+            data_dic['taxiLatitude']=taxiLatitude
+            data_dic['taxiLongitude']=taxiLongitude
+            data_dic['criminalCode']=criminalCode
+            data_dic['criminalName']=criminalName
+            data_dic['criminalBirth']=criminalBirth
+            data_dic['criminalGender']=criminalGender
+            data_dic['criminalHeight']=criminalHeight
+            data_dic['criminalAddress']=criminalAddress
+            data_dic['payCode']=payCode
+            data_dic['payInfo']=payInfo
+            data_dic['paySum']=paySum
+
+
+            return render(req, 'blog/post_list.html',data_dic)
+
+        count=0
+        return render(req, 'blog/post_list.html',{})
